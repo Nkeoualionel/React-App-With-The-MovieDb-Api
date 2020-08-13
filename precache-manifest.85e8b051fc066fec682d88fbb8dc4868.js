7ff713625249eaf29f472a6b759ffb76e9fa38ae
Lionel NKEOUA <mbembasachat9@gmail.com>
2020-08-13 18:37:22 +0000

    Updates

---
+++ precache-manifest.85e8b051fc066fec682d88fbb8dc4868.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95ca89145543f6f8ab6e9b4c7823e8e1",
+    "url": "/React-App-With-The-MovieDb-Api/index.html"
+  },
+  {
+    "revision": "60b2be40bc4afc04d888",
+    "url": "/React-App-With-The-MovieDb-Api/static/css/main.47470f99.chunk.css"
+  },
+  {
+    "revision": "e2fa71b1b86c8839961c",
+    "url": "/React-App-With-The-MovieDb-Api/static/js/2.5ef0a5d7.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/React-App-With-The-MovieDb-Api/static/js/2.5ef0a5d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60b2be40bc4afc04d888",
+    "url": "/React-App-With-The-MovieDb-Api/static/js/main.d63d6c6a.chunk.js"
+  },
+  {
+    "revision": "e862fc6fb98b3e50e71a",
+    "url": "/React-App-With-The-MovieDb-Api/static/js/runtime-main.0db92b28.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-App-With-The-MovieDb-Api/static/media/logo.5d5d9eef.svg"
+  }
+]);
